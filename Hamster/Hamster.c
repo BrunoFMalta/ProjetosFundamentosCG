2b966e151aa399821d1e6c6e4753e30d1e93a457
Bruno Felipe Malta <brunofmalta12@gmail.com>
2024-10-23 19:37:57 -0300

    Add files via upload

---
+++ Hamster/Hamster.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h> 
+
+
+void position(int posiHamster1, int posiHamster2) {
+    printf("H1: ");
+    for (int i = 0; i < posiHamster1; i++) {
+        printf("* ");
+    }
+    printf("\n");
+
+    printf("H2: ");
+    for (int i = 0; i < posiHamster2; i++) {
+        printf("* ");
+    }
+}
+
+int main() {
+    srand(time(0)); 
+
+    int posiHamster1 = 0;
+    int posiHamster2 = 0;  
+    bool end = false;  
+
+    while (!end) { 
+       
+        int n1 = rand() % 5 + 1;
+        if (n1 == 1) {
+            posiHamster1++;
+        } else if (n1 == 2) {
+            posiHamster1 += 2;
+        } else if (n1 == 4) {
+            posiHamster1--;
+        } else if (n1 == 5) {
+            posiHamster1 -= 2;
+        }
+
+        if (posiHamster1 < 0) {
+            posiHamster1 = 0;
+        }
+
+        int n2 = rand() % 5 + 1;
+        if (n2 == 1) {
+            posiHamster2++;
+        } else if (n2 == 2) {
+            posiHamster2 += 2;
+        } else if (n2 == 4) {
+            posiHamster2--;
+        } else if (n2 == 5) {
+            posiHamster2 -= 2;
+        }
+
+        if (posiHamster2 < 0) {
+            posiHamster2 = 0;
+        }
+
+        position(posiHamster1, posiHamster2);
+
+        if (posiHamster1 >= 12 && posiHamster2 >= 12) {
+            printf("Empate!!\n");
+            end = 1;
+        } else if (posiHamster1 >= 12) {
+            printf("Hamster 1 Venceu!!\n");
+            end = 1;
+        } else if (posiHamster2 >= 12) {
+            printf("Hamster 2 Venceu!!\n");
+            end = 1;
+        }
+    }
+
+    printf("The race has ended!\n");
+    return 0;
+}
